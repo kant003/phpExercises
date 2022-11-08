cc451d83d026f1a0f66df50c57a26b4dfa5c17d1
kant003 <gonzalezm.angel@gmail.com>
2022-11-08 10:51:49 +0100

    add 9 exercise

---
+++ ejer09.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Crea una clase que permita almacenar los datos de un animal
+ * En concreto tendremos que guardar:
+ * - Nombre (texto)
+ * - Edad (numérico)
+ * - Si es salvaje (booleano)
+ * - peso(numérocp)
+ * La clase tendrá un método que determine si el animal es peligroso
+ * - Será peligroso cuando su peso supere los 10 kilos y sea salvaje
+ * Añade un constructor a la clase
+ * 
+ * Por último, crea un objeto de tipo Animal
+ * - Muestra por pantalla sus datos
+ */
+
+?>
