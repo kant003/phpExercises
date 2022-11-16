ee8c72d17a99f662e3e3ab1481efdf5ee0cd9762
kant003 <gonzalezm.angel@gmail.com>
2022-11-16 12:30:36 +0100

    ejer12

---
+++ ejer12.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Crea una función a la que se le pase 
+ * una lista de numeros (array) y un numero
+ * y nos diga si ese número esta dentro del array
+ * 
+  */
+function isOnTheList(list, vale){
+    
+}
+?>
