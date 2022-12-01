ca7c2a2d2b14f9674d2c866599e1756cb6bce250
kant003 <gonzalezm.angel@gmail.com>
2022-12-01 10:22:34 +0100

    ejer18

---
+++ ejer18.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Basandote en el ejercicio anterior, ejer17
+ * Crea una función que devuelva la Clasificación del IMC de la OMS
+ * Delgadez severa, Delgadez moderada, Delgadez leve, ...
+ * Mira la wikipedia
+ */
+
+function getClasification($peso, $altura){
+    
+}
+
+?>
+
+
