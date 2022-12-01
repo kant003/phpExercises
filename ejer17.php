c97b5b3a83a98f7aa0421740bf5ff87388a05878
kant003 <gonzalezm.angel@gmail.com>
2022-12-01 10:19:28 +0100

    ejer17

---
+++ ejer17.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Crea un programa que calcule el IMC de una persona
+ * Busca en la wikipedia como se calcula
+ * La función se llamará getIMC y recibirá 2 parametros el peso y la altura
+ * la función devolverá un valor decimal
+ */
+?>
