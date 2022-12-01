3572edba5cd453333fa7ee14ba1077d103e35794
kant003 <gonzalezm.angel@gmail.com>
2022-12-01 10:36:47 +0100

    ejer20 y 21

---
+++ ejer20.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Crea una pagina web en PHP que reciba por $GET 2 valores
+ * El programa los multiplicará y mostrará el resultado por pantalla
+*/
+ 
+
+?>
---
+++ ejer21.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Crea una función que reciba un array 
+ * y devuelva uno de los elementos de ese array de forma aleatoria
+ * 
+*/
+ function getRandomValue($list){
+    
+ }
+
+?>
