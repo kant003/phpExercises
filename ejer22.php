924333afc220506a027d5ba87534eef7fc56cd82
kant003 <gonzalezm.angel@gmail.com>
2022-12-01 10:49:56 +0100

    ejer22

---
+++ ejer22.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Crea un programa que te indique cuantos días faltan para navidad
+ * PISTA: puedes usar la funcion diff de fechas en php
+*/
+ function getDaysToChristmas(){
+    
+ }
+
+?>
