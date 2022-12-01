8b7b42a06df11d9e97f416de6e946e241a7b4ca3
kant003 <gonzalezm.angel@gmail.com>
2022-12-01 10:29:10 +0100

    ejer19

---
+++ ejer19.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Pinta un arbol de navidad en pantalla usando caracteres assci
+ * A la función se le pasará el parametro de la altura
+ * el parametro algura solo podrá ser un número impar
+
+ ej: Arbol con altura 3
+  *
+ ***
+*****
+
+
+ ej: Arbol con altura 5
+    *
+   ***
+  *****
+ *******
+*********
+
+ */
+
+function drawChristmasTree($altura){
+    
+}
+
+?>
+
+
