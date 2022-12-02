eb596bb24fda77ca90eef89d3493ba781e961aad
kant003 <gonzalezm.angel@gmail.com>
2022-12-02 10:37:25 +0100

    ejer 23

---
+++ ejer23.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Crea un programa que muestre por pantalla el área y el perímetro de un círculo de radio R
+ * Para ello crea una clase Circle
+ * Esta clase tendrá los atributos radio y color
+ * Esta clase tendrá también 2 metodos getArea y getPerimeter que calcularán respectivamente el área y el perímetro
+ * Busca en internet cual es la formula para realizar los cálculos
+ * ------------------------------------------
+ * Crea un formulario html que el usuario pueda rellenar, con el campo (input) radio y un boton submit
+ * Cuando el usuario envie el formulario se mostrará por pantalla el área y perímetro de ese círculo
+*/
+
+
+?>
